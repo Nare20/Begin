c42a996bd52669037964cc1b346aee5becb7df28
Nare Grigoryan <naregrigoryan15@gmail.com>
2026-03-04 22:44:44 +0400

    Add files via upload

---
+++ pordz.cpp
@@ -0,0 +1,72 @@
+//   first code
+/*#include<iostream>
+using namespace std;
+int main(){
+    int a, b, c;
+    cout << "Enter first number\n";
+    cin >> a;
+    cout << "Enter second number\n";
+    cin >> b;
+    c = a+b;
+    cout << " Add= " << c;
+}
+//    /n
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout << "Enter first number \n a = ";
+    cin >> a;
+    cout << "Enter second number /n b = ;";
+    cin >> b;
+    c = a-b;
+    cout << " Sub = " << c;*/
+  //                types  
+
+  //   setprecsion pordz
+#include <iostream>
+#include<iomanip>
+using namespace std;
+int main() {
+    int a = 3;
+    long long  b = 12345678912345;
+    char s = 'a';
+    float g = 334.230;
+    double d = 14049.304930000;
+    cout << a << "\n" << b << "\n" << s << "\n" << g << "\n" << fixed << setprecision(9) << d;
+    return 0;
+}
+//    string
+/*#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+string a = "This is my first program in C++ ";
+cout << a << endl;
+ a = "This is my secong program in C++ ";
+cout << a << endl;
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
